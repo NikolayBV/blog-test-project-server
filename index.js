8a6eb1699511bcebf7150045434a1ceb6781d3c6
NikolayBV <bnv95o@yandex.ru>
2022-10-16 13:14:34 +0600

    changes-api

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import cors from 'cors';
 const app = express();
 const corsOptions ={
     origin:'http://localhost:3000',
-    credentials:true,            //access-control-allow-credentials:true
+    credentials:true,
     optionSuccessStatus:200
 }
 app.use(cors(corsOptions));
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 
 
 let arrayPosts = posts;
-const createPath = (page) => path.resolve(__dirname, 'dist', `${page}.html`);
+let arrayUsers = users;
 const PORT = 5000;
 
 app.listen(PORT, (err) => {
@@ -28,62 +28,82 @@ app.get('/', (req, res) => {
 })
 
 app.get('/posts', (req, res) => {
-    if(req.query){
+    if(Object.keys(req.query).length !== 0){
         const pag = req.query.page;
         const lim = req.query.limit;
-        if(lim == '10'){
-            if(pag == '1') res.send(arrayPosts.slice(pag - 1, lim))
-            else{
-                const start = (pag - 1) * lim;
-                const end = +start + +lim;
-                res.send(arrayPosts.slice(start, end));
-            }
-        }
+
+        const start = (pag - 1) * lim;
+        const end = +start + +lim;
+        res.send(arrayPosts.slice(start, end));
     }
-    else{
+    else if(Object.keys(req.query).length === 0){
         res.send(arrayPosts);
     }
-});
-app.get('/posts/all', (req, res) => {
-    res.send(arrayPosts);
+
+    //todo: удалить условия
 });
 
+
 app.get('/users', (req, res) => {
-    res.send(users);
+    res.send(arrayUsers);
 });
 
 app.get('/posts/:id', (req, res) => {
     let id = req.params.id;
-    res.send(arrayPosts[id - 1])//переписать на find
+    let post = arrayPosts.find(item => item.id == id)
+    res.send(post)
+    //todo: переписать на find
 });
 
 app.get('/users/:id', (req, res) => {
     let id = req.params.id;
-    res.send(users[id - 1])
+    res.send(arrayUsers.find(item => item.id === id))
+    //todo: переписать на find
 });
 
-app.get('/posts/:id/:user', (req, res) => {
-    let id = req.params.id;
-    let user = users.find((item) => {
-        return item.id == posts[id].userId;
-    });
-    res.send(user);
-});
+// app.get('/posts/:id/:user', (req, res) => {
+//     let id = req.params.id;
+//     let user = users.find((item) => {
+//         return item.id == posts[id].userId;
+//     });
+//     res.send(user);
+// });
 
-app.post('/posts/:id', (req, res) => {
-    arrayPosts = arrayPosts.map((post) => {
-        if(post.id == req.body.id){
-            post.title = req.body.title
-            post.body = req.body.body
-        }
-        return post;
-    })
-    res.sendStatus(200);
+app.put('/posts/:id', (req, res) => {
+    try{
+        arrayPosts = arrayPosts.map((post) => {
+            if(post.id === req.body.id){
+                post.id = req.body.id
+                post.title = req.body.title
+                post.body = req.body.body
+            }
+            return post;
+        })
+        //todo: использовать find и обработать ошибку, сгенерировать новый id;
+        //post - добавление, put - измененеи
+        res.sendStatus(200);
+    }
+    catch (e){
+        res.sendStatus(404);
+    }
 })
 
-app.post('/posts/add/:id', (req, res) => {
-    const post = {
-        id: req.body.id,
+
+// app.post('/posts/add/:id', (req, res) => {
+//     const post = {
+//         id: req.body.id,
+//         title: req.body.title,
+//         body: req.body.body,
+//         author: req.body.author
+//     }
+//     arrayPosts = [...arrayPosts, post];
+//     res.sendStatus(200);
+// })
+
+// todo: app.put();
+app.put('/create', (req, res) => {
+    const post= {
+        id: Date.now(),
         title: req.body.title,
         body: req.body.body,
         author: req.body.author
