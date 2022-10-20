d753a4d247d6750367bf67e7252a8c75ac836f36
NikolayBV <bnv95o@yandex.ru>
2022-10-20 18:45:27 +0600

    add-ts

--- index.js
+++ index.js
@@ -1,139 +1,137 @@
-import path from 'path';
-import express from 'express';
-import posts from './data/posts.json' assert {type: 'json'};
-import users from './data/users.json' assert {type: 'json'};
-import cors from 'cors';
-import fs from 'fs';
-
-const app = express();
-const corsOptions ={
-    origin:'http://localhost:3000',
-    credentials:true,
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-
-let arrayPosts = posts;
-let arrayUsers = users;
-
+"use strict";
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+exports.__esModule = true;
+var express_1 = require("express");
+var posts_json_1 = require("./data/posts.json");
+var users_json_1 = require("./data/users.json");
+var cors_1 = require("cors");
+var fs_1 = require("fs");
+var app = (0, express_1["default"])();
+var corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true,
+    optionSuccessStatus: 200
+};
+app.use((0, cors_1["default"])(corsOptions));
+app.use(express_1["default"].json());
+app.use(express_1["default"].urlencoded({ extended: true }));
+var arrayPosts = posts_json_1["default"];
+var arrayUsers = users_json_1["default"];
 //todo: изменять сами файлы;
-
-const PORT = 5000;
-
-app.listen(PORT, (err) => {
-    err ? console.log(err): console.log(`listening port ${PORT}`);
+var PORT = 5000;
+app.listen(PORT, function () {
+    console.log("listening port ".concat(PORT));
 });
-
-app.get('/', (req, res) => {
+app.get('/', function (req, res) {
     res.send(arrayPosts);
-})
-
-app.get('/posts', (req, res) => {
-    if(Object.keys(req.query).length !== 0){
-        const pag = req.query.page;
-        const lim = req.query.limit;
-
-        const start = (pag - 1) * lim;
-        const end = +start + +lim;
-        res.send({posts: arrayPosts.slice(start, end), count: posts.length});
+});
+app.get('/posts', function (req, res) {
+    if (Object.keys(req.query).length !== 0) {
+        var pag = String(req.query.page);
+        var lim = String(req.query.limit);
+        var start = (Number(pag) - 1) * Number(lim);
+        var end = +start + +lim;
+        res.send({ posts: arrayPosts.slice(start, end), count: posts_json_1["default"].length });
     }
-    else{
-        res.send({posts: arrayPosts, count: posts.length});
+    else {
+        res.send({ posts: arrayPosts, count: posts_json_1["default"].length });
     }
-
     //todo: удалить условия - done
 });
-
-app.get('/posts/:id', (req, res) => {
-    let id = req.params.id;
-    let post = arrayPosts.find(item => item.id == id)
-    res.send(post)
+app.get('/posts/:id', function (req, res) {
+    var id = req.params.id;
+    var post = arrayPosts.filter(function (item) { return item.id === Number(id); }); //use filter - not find
+    res.send(post);
     //todo: переписать на find - done
 });
-
-app.post('/posts/:id', (req, res) => {
-    arrayPosts = arrayPosts.map((item) => {
-            if(item.id === req.body.id){
-                item.id = req.body.id
-                item.title = req.body.title
-                item.body = req.body.body
-                return item;
-            }
-            return item;
-    });
-    fs.writeFile('./data/posts.json', JSON.stringify(arrayPosts), (err) => {
-        if(err) res.sendStatus(404)
-        res.send(req.body);
-    })
-});
-
-app.delete('/posts/:id',(req, res) => {
-    const id = req.params.id;
-    arrayPosts = arrayPosts.filter((post) => {
-        return String(post.id) !== id;
-    })
-    fs.writeFile('./data/posts.json', JSON.stringify(arrayPosts), (err) => {
-        if(err) res.sendStatus(404)
-        res.send(req.body);
-    })
-});
-
-app.get('/users', (req, res) => {
-    res.send(arrayUsers);
-});
-
-app.get('/users/:id', (req, res) => {
-    let id = req.params.id;
-    res.send(arrayUsers.find(item => item.id === id))
-    //todo: переписать на find
-});
-
-app.post('/users/:id', (req, res) => {
-    arrayUsers = arrayUsers.map((item) => {
-        if(item.id === req.body.id){
-            item.id = req.body.id
-            item.title = req.body.title
-            item.body = req.body.body
+app.post('/posts/:id', function (req, res) {
+    arrayPosts = arrayPosts.map(function (item) {
+        if (item.id === req.body.id) {
+            item.id = req.body.id;
+            item.title = req.body.title;
+            item.body = req.body.body;
             return item;
         }
         return item;
     });
-    fs.writeFile('./data/users.json', JSON.stringify(arrayUsers), (err) => {
-        if(err) res.sendStatus(404)
-        res.send(req.body);
-    })
+    fs_1["default"].writeFile('./data/posts.json', JSON.stringify(arrayPosts), function (err) {
+        if (err)
+            res.sendStatus(404);
+        var myObj = req.body;
+        res.send(myObj);
+    });
 });
-
-app.delete('/users/:id',(req, res) => {
-    const id = req.params.id;
-    arrayPosts = arrayPosts.filter((user) => {
+app["delete"]('/posts/:id', function (req, res) {
+    var id = req.params.id;
+    arrayPosts = arrayPosts.filter(function (post) {
+        return String(post.id) !== id;
+    });
+    fs_1["default"].writeFile('./data/posts.json', JSON.stringify(arrayPosts), function (err) {
+        if (err)
+            res.sendStatus(404);
+        var myObj = req.body;
+        res.send(myObj);
+    });
+});
+app.get('/users', function (req, res) {
+    res.send(arrayUsers);
+});
+app.get('/users/:id', function (req, res) {
+    var id = req.params.id;
+    res.send(arrayUsers.find(function (item) { return item.id === Number(id); }));
+    //todo: переписать на find
+});
+app.post('/users/:id', function (req, res) {
+    arrayUsers = arrayUsers.map(function (item) {
+        if (item.id === req.body.id) {
+            //do something
+            return item;
+        }
+        return item;
+    });
+    fs_1["default"].writeFile('./data/users.json', JSON.stringify(arrayUsers), function (err) {
+        if (err)
+            res.sendStatus(404);
+        var myObj = req.body;
+        res.send(myObj);
+    });
+});
+app["delete"]('/users/:id', function (req, res) {
+    var id = req.params.id;
+    arrayPosts = arrayPosts.filter(function (user) {
         return String(user.id) !== id;
-    })
-    fs.writeFile('./data/users.json', JSON.stringify(arrayUsers), (err) => {
-        if(err) res.sendStatus(404)
-        res.send(req.body);
-    })
+    });
+    fs_1["default"].writeFile('./data/users.json', JSON.stringify(arrayUsers), function (err) {
+        if (err)
+            res.sendStatus(404);
+        var myObj = req.body;
+        res.send(myObj);
+    });
 });
-
-
-
 // todo: app.put();
-app.put('/posts', (req, res) => {
-    const findPost = users.find(user => user.name === req.body.author);
-    if(!findPost) res.send(404);
-    const post= {
+app.put('/posts', function (req, res) {
+    var findPost = users_json_1["default"].find(function (user) { return user.name === req.body.author; });
+    if (!findPost)
+        res.send(404);
+    var post = {
         id: Date.now(),
         title: req.body.title,
         body: req.body.body,
         author: req.body.author
-    }
-    arrayPosts = [...arrayPosts, post];
-    fs.writeFile('./data/posts.json', JSON.stringify(arrayPosts), (err) => {
-        if(err) res.sendStatus(404)
-        res.send(req.body);
-    })
-})
-
+    };
+    arrayPosts = __spreadArray(__spreadArray([], arrayPosts, true), [post], false);
+    fs_1["default"].writeFile('./data/posts.json', JSON.stringify(arrayPosts), function (err) {
+        if (err)
+            res.sendStatus(404);
+        var myObj = req.body;
+        res.send(myObj);
+    });
+});
---
+++ index.ts
@@ -0,0 +1,148 @@
+import express from "express"
+import posts from "./data/posts.json";
+import users from "./data/users.json";
+import cors from "cors";
+import fs from "fs";
+import {IPost, IUser} from "./data/models/models";
+
+const app = express();
+const corsOptions ={
+    origin:'http://localhost:3000',
+    credentials:true,
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+let arrayPosts = posts;
+let arrayUsers = users;
+
+//todo: изменять сами файлы;
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+    console.log(`listening port ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+    res.send(arrayPosts);
+})
+
+interface PostsObjSend{
+    posts: Array<IPost>,
+    count: number
+}
+
+app.get<PostsObjSend>('/posts', (req, res) => {
+    if(Object.keys(req.query).length !== 0){
+        const pag: string = String(req.query.page);
+        const lim: string = String(req.query.limit);
+
+        const start: number = (Number(pag) - 1) * Number(lim);
+        const end: number = +start + +lim;
+        res.send({posts: arrayPosts.slice(start, end), count: posts.length});
+    }
+    else{
+        res.send({posts: arrayPosts, count: posts.length});
+    }
+
+    //todo: удалить условия - done
+});
+
+
+app.get('/posts/:id', (req, res) => {
+    let id = req.params.id;
+    let post: Array<IPost> = arrayPosts.filter((item: IPost) => item.id === Number(id)) //use filter - not find
+    res.send(post)
+    //todo: переписать на find - done
+});
+
+app.post('/posts/:id', (req, res) => {
+    arrayPosts = arrayPosts.map((item) => {
+            if(item.id === req.body.id){
+                item.id = req.body.id
+                item.title = req.body.title
+                item.body = req.body.body
+                return item;
+            }
+            return item;
+    });
+    fs.writeFile('./data/posts.json', JSON.stringify(arrayPosts), (err) => {
+        if(err) res.sendStatus(404)
+        const myObj: object = req.body;
+        res.send(myObj)
+    })
+});
+
+app.delete('/posts/:id',(req, res) => {
+    const id: string = req.params.id;
+    arrayPosts = arrayPosts.filter((post: IPost) => {
+        return String(post.id) !== id;
+    })
+    fs.writeFile('./data/posts.json', JSON.stringify(arrayPosts), (err) => {
+        if(err) res.sendStatus(404)
+        const myObj: object = req.body;
+        res.send(myObj);
+    })
+});
+
+app.get<Array<IUser>>('/users', (req, res) => {
+    res.send(arrayUsers);
+});
+
+app.get('/users/:id', (req, res) => {
+    let id = req.params.id;
+    res.send(arrayUsers.find((item) => item.id === Number(id)))
+    //todo: переписать на find
+});
+
+app.post('/users/:id', (req, res) => {
+    arrayUsers = arrayUsers.map((item) => {
+        if(item.id === req.body.id){
+            //do something
+            return item;
+        }
+        return item;
+    });
+    fs.writeFile('./data/users.json', JSON.stringify(arrayUsers), (err) => {
+        if(err) res.sendStatus(404)
+        const myObj: object = req.body;
+        res.send(myObj)
+    })
+});
+
+app.delete('/users/:id',(req, res) => {
+    const id = req.params.id;
+    arrayPosts = arrayPosts.filter((user) => {
+        return String(user.id) !== id;
+    })
+    fs.writeFile('./data/users.json', JSON.stringify(arrayUsers), (err) => {
+        if(err) res.sendStatus(404)
+        const myObj: object = req.body;
+        res.send(myObj)
+    })
+});
+
+
+
+// todo: app.put();
+app.put('/posts', (req, res) => {
+    const findPost = users.find((user) => user.name === req.body.author);
+    if(!findPost) res.send(404);
+    const post = {
+        id: Date.now(),
+        title: req.body.title,
+        body: req.body.body,
+        author: req.body.author
+    }
+    arrayPosts = [...arrayPosts, post];
+    fs.writeFile('./data/posts.json', JSON.stringify(arrayPosts), (err) => {
+        if(err) res.sendStatus(404)
+        const myObj: object = req.body;
+        res.send(myObj)
+    })
+})
+
